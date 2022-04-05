d08c969e780d2e7405d1908d2dbe3424f060daff
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-04-05 19:54:17 +0700

    styled-components

--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { HeaderRight, HeaderStyle, LogoStyle } from '@/styles/HeaderStyles'
+import { Container } from '@/styles/Containers'
+import Logo from '@/components/shared/Logo'
+import Link from 'next/link'
+import Search from '@/components/shared/Search'
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
+interface HeaderProps {}
+
+const Header: React.FC<HeaderProps> = () => {
+   return (
+      <HeaderStyle>
+         <Container>
+            <LogoStyle>
+               <Link href="/">
+                  <a><Logo /></a>
+               </Link>
+            </LogoStyle>
+
+            <HeaderRight>
+               <Search />
+            </HeaderRight>
+            
+         </Container>   
+      </HeaderStyle>
+   )
 }
 
 export default Header
