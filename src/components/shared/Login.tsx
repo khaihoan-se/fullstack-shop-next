ac43db6d3b1c225d2e1130978b4818126d12f50c
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-04-12 18:23:52 +0700

    Login Form

--- src/components/shared/Login.tsx
+++ src/components/shared/Login.tsx
@@ -1,20 +1,37 @@
 import React from "react";
 import styles from '@/styles/Login.module.scss'
-import CloseIcon from '@/components/icons/CloseIcon'
 import Module from "@/components/shared/Module";
 
 interface Props {
     openLogin: boolean,
-    setOpenLogin: (open: boolean) => void;
+    setOpenLogin: (open: boolean) => void,
+    titleLogin: string
 }
+const titleLogin = 'Log in to Akame'
 
 const Login: React.FC<Props> = ({ openLogin, setOpenLogin }) => {
     return (
-        <div className={styles.login}>
-           <Module>
-               hello
-           </Module>
-        </div>
+        <>
+            <Module openLogin={openLogin} setOpenLogin={setOpenLogin} title={titleLogin}>
+                <div className={styles.login}>
+                    <div className={styles.login__item}>
+                        <div className={styles.login__item__label}>Email address or phone number</div>
+                        <div className={styles.login__item__input}>
+                            <input type="text" />
+                        </div>
+                    </div>
+                    <div className={styles.login__item}>
+                        <div className={styles.login__item__label}>Password</div>
+                        <div className={styles.login__item__input}>
+                            <input type="text" />
+                        </div>
+                    </div>
+                    <div className={styles.login__item}>
+                        Submit
+                    </div>
+                </div>
+            </Module>
+        </>
     )
 }
 
--- src/components/shared/Module.tsx
+++ src/components/shared/Module.tsx
@@ -2,16 +2,26 @@ import React from "react";
 import styles from '@/styles/Module.module.scss'
 import CloseIcon from '@/components/icons/CloseIcon'
 
-const Module: React.FC = ({children}) => {
+interface Props {
+    openLogin: boolean,
+    setOpenLogin: (open: boolean) => void;
+    title: string
+}
+const Module: React.FC<Props> = ({ openLogin, setOpenLogin, title, children}) => {
     return (
         <div className={styles.module}>
-            <div className={styles.module__close}>
-                <CloseIcon />
-            </div>
             <div className={styles.module__main}>
-                {
-                    children
-                }
+                <div className={styles.module__header}>
+                    <div className={styles.module__header__title}>{title}</div>
+                    <div className={styles.module__header__close} onClick={() => setOpenLogin(!openLogin)}>
+                        <CloseIcon />
+                    </div>
+                </div>
+                <div className={styles.module__body}>
+                    {
+                        children
+                    }
+                </div>
             </div>
         </div>
     )
